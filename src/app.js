be2efe5fd0b5ffdbe3b4eee369802eb56ccdeaf0
phucnn1707 <19520224@gm.uit.edu.vn>
2024-08-21 17:18:28 +0700

    initial nodejs

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const { connectDB } = require('./config/db.config');
+const exampleRoutes = require('./routes/example.routes');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// Database connection
+connectDB();
+
+// Use routes
+app.use('/api', exampleRoutes);
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
